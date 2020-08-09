14d7920dc2cd94867f7681f6730b44a24dbba311
Iconews <pedrocasella27@gmail.com>
2020-08-09 00:52:17 -0300

    small update

--- js/trojan.js
+++ js/trojan.js
@@ -6,8 +6,12 @@ function acesso(form){
     
         form.nome.value = form.nome.value.toLowerCase ()
         form.senha.value = form.senha.value.toLowerCase ()
-        if (form.nome.value == "pedro casella" && form.senha.value == "icc2018133" || form.nome.value == "ruan paixão" && form.senha.value == "eshelleytesta" || form.nome.value == "admin" && form.senha.value == "admin123" || form.nome.value == "marcus fragale" && form.senha.value == "iccprofessor" || form.nome.value == "fernando venâncio" && form.senha.value == "iccdiretor" || form.nome.value == "cauã murta" && form.senha.value == "batataquente" || form.nome.value == "lara beringuy" && form.senha.value == "formiguinha" || form.nome.value == "fabiano couso" && form.senha.value == "icc2018254" || form.nome.value == "igor abreu" && form.senha.value == "icc201808" ) {
-          location = "socio/paginasocio.html" }
+        if (form.nome.value == "pedro casella" && form.senha.value == "icc2018133" ) {
+          location = "socio/paginasociopedro.html" }
+
+        if (form.nome.value == "ruan souza" && form.senha.value == "eshelleytesta" ) {
+          location = "socio/paginasocioruan.html" }
+          
         else {
           form.nome.value=""
           form.senha.value=""
